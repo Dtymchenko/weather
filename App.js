128a0fa598310fed9c41634cfec6e11e1965c3b6
Dmytro Tymchenko <timchenkod88@gmail.com>
2022-12-11 20:54:31 +0200

    already received weather by coords from API

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Alert } from "react-native";
+import * as Location from "expo-location";
+import axios from "axios";
+import React from "react";
+import Loading from "./.expo/components/Loading";
+import Weather from "./.expo/components/Weather";
 
 export default function App() {
+  const API_KEY = "fd881d906a7e39a1f689ef8ade4b4ba4";
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [weather, setWeather] = React.useState({});
+
+  async function getWeather(lat, long) {
+    const res = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
+    );
+    setWeather(res.data);
+    // console.log(weather);
+  }
+
+  async function getLocation() {
+    try {
+      setIsLoading(true);
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't define location", error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  React.useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="dark-content" hidden="false" />
+      {isLoading ? <Loading /> : <Weather weather={weather} />}
     </View>
   );
 }
@@ -13,8 +47,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
   },
 });
